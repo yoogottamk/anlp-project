36db4066b96696598117c07757a39b1da0cf70aa
yoogottamk <yoogottamk@outlook.com>
2021-12-08 20:08:14 +0530

    minor improvements to convert-to-jsonl

--- anlp_project/datasets/convert-to-jsonl.py
+++ anlp_project/datasets/convert-to-jsonl.py
@@ -1,12 +1,15 @@
+import json
+
+from tqdm import tqdm
+
 from anlp_project.datasets.europarl import EuroParlRaw
 
 
 def main():
     dataset = EuroParlRaw()
-    with open('dataset.jsonl', 'w') as f:
-        for i in range(len(dataset)):
-            de, en = dataset[i]
-            f.write(f'{{ "translation": {{ "en": "{en}", "de": "{de}" }} }}\n')
+    with open("dataset.jsonl", "w") as f:
+        for de, en in tqdm(dataset):
+            f.write(json.dumps({"translation": {"en": en, "de": de}}) + "\n")
 
 
 if __name__ == "__main__":
