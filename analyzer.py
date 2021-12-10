902193f10ed5c261df068a35656c7243492bb371
Gaurang Tandon <1gaurangtandon@gmail.com>
2021-12-10 21:15:41 +0530

    Added graphs with code

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,7 @@
 #/usr/bin/python3
 
 from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
 import os
 import sys
@@ -37,10 +38,16 @@ def make_plots():
             except ValueError:
                 invalid_sentence_count += 1
 
+    token_diff = [de_counts[i] - en_counts[i] for i in range(len(de_counts))]
+    token_diff.sort()
+    token_series = pd.Series(token_diff, dtype=np.int64)
+
     de_counts.sort()
     en_counts.sort()
-    de_series = pd.Series(de_counts, dtype=int)
-    en_series = pd.Series(en_counts, dtype=int)
+    de_series = pd.Series(de_counts, dtype=np.int64)
+    en_series = pd.Series(en_counts, dtype=np.int64)
+
+    line_colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'purple']
 
     # token_counts[0].sort()
     # token_counts[1].sort()
@@ -48,15 +55,28 @@ def make_plots():
 
     print("Statistics on the token counts")
     print("German")
-    print(de_series.describe())
+    print(de_series.describe().astype(np.int64))
     print("English")
-    print(en_series.describe())
+    print(en_series.describe().astype(np.int64))
 
-    line_colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+    print("Token count differences")
+    print(token_series.describe().astype(np.int64))
+    print("Token difference 2% quantile", token_series.quantile(q=0.02))
+    print("Token difference 10% quantile", token_series.quantile(q=0.1))
+    print("Token difference 90% quantile", token_series.quantile(q=0.9))
+    print("Token difference 97% quantile", token_series.quantile(q=0.97))
+    plt.xlabel("Index of sentence pair")
+    plt.ylabel("Difference value")
+    plt.plot(token_series, label="German token count - English token count", color=line_colors[-1])
+    plt.legend()
+    plt.show()
+
     for language in languages:
         language_label = "English" if language == "en" else "German"
 
         plot_series = de_series if language == "de" else en_series
+        plt.xlabel("Index of sentence pair")
+        plt.ylabel("Token count")
         plt.plot(plot_series, label=f"{language_label} token counts", color=line_colors[0]) 
 
         for index, quantile in enumerate([0.5, 0.75, 0.95, 0.99, 0.9999]):
