a2195802ff496885cb13b953e1f4692ed461ed0d
Gaurang Tandon <1gaurangtandon@gmail.com>
2021-12-11 18:57:07 +0530

    Refactored out more unused branches

--- anlp_project/models/transformer.py
+++ anlp_project/models/transformer.py
@@ -2,15 +2,12 @@
 # examples script provided in the huggingface repo
 # link: https://tinyurl.com/runtranslationhf
 
-import logging
 import os
-import sys
 from dataclasses import dataclass, field
 from typing import Optional
 
 import datasets
-import numpy as np
-from datasets import load_dataset, load_metric
+from datasets import load_dataset
 from transformers import (
     AutoConfig,
     AutoModelForSeq2SeqLM,
@@ -19,47 +16,35 @@ from transformers import (
     HfArgumentParser,
     Seq2SeqTrainer,
     Seq2SeqTrainingArguments,
-    default_data_collator,
     set_seed,
 )
 from transformers.trainer_utils import get_last_checkpoint
 
-logger = logging.getLogger(__name__)
-
 
 @dataclass
 class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
     model_name_or_path: str = field()
     config_name: Optional[str] = field(default=None)
     tokenizer_name: Optional[str] = field(default=None)
-    cache_dir: Optional[str] = field(
-        default=None,
-    )
 
 
 @dataclass
 class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
     source_lang: str = field(default=None)
     target_lang: str = field(default=None)
 
-    dataset_name: Optional[str] = field(default=None)
-    dataset_config_name: Optional[str] = field(default=None)
     train_file: Optional[str] = field(default=None)
     max_source_length: Optional[int] = field(default=1024)
     max_target_length: Optional[int] = field(
         default=128,
     )
-    pad_to_max_length: bool = field(
-        default=False,
-    )
-    max_train_samples: Optional[int] = field(default=None)
-    ignore_pad_token_for_loss: bool = field(
-        default=True,
-    )
     source_prefix: Optional[str] = field(default=None)
-    forced_bos_token: Optional[str] = field(
-        default=None,
-    )
 
 
 def main():
@@ -68,23 +53,8 @@ def main():
     )
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
-    # Setup logging
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-        datefmt="%m/%d/%Y %H:%M:%S",
-        handlers=[logging.StreamHandler(sys.stdout)],
-    )
-
     log_level = training_args.get_process_log_level()
-    logger.setLevel(log_level)
     datasets.utils.logging.set_verbosity(log_level)
-
-    # Log on each process the small summary:
-    logger.warning(
-        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
-        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
-    )
-    logger.info(f"Training/evaluation parameters {training_args}")
 
     # Detecting last checkpoint.
     last_checkpoint = None
@@ -94,43 +64,25 @@ def main():
         and not training_args.overwrite_output_dir
     ):
         last_checkpoint = get_last_checkpoint(training_args.output_dir)
-        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
-            raise ValueError(
-                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
-                "Use --overwrite_output_dir to overcome."
-            )
-        elif (
-            last_checkpoint is not None and training_args.resume_from_checkpoint is None
-        ):
-            logger.info(
-                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
-                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
-            )
 
     # Set seed before initializing model.
     set_seed(training_args.seed)
 
-    # Get the datasets: you can either provide your own JSON training and evaluation files (see below)
-    # For translation, only JSON files are supported, with one field named "translation" containing two keys for the
-    # source and target languages (unless you adapt what follows).
     data_files = {"train": data_args.train_file}
     extension = "json"
     raw_datasets = load_dataset(
-        extension, data_files=data_files, cache_dir=model_args.cache_dir
+        extension, data_files=data_files
     )
 
     config = AutoConfig.from_pretrained(
         model_args.model_name_or_path,
-        cache_dir=model_args.cache_dir,
     )
     tokenizer = AutoTokenizer.from_pretrained(
         model_args.model_name_or_path,
-        cache_dir=model_args.cache_dir,
     )
     model = AutoModelForSeq2SeqLM.from_pretrained(
         model_args.model_name_or_path,
         config=config,
-        cache_dir=model_args.cache_dir,
     )
 
     model.resize_token_embeddings(len(tokenizer))
@@ -147,15 +99,6 @@ def main():
 
     # Temporarily set max_target_length for training.
     max_target_length = data_args.max_target_length
-    padding = "max_length" if data_args.pad_to_max_length else False
-
-    if training_args.label_smoothing_factor > 0 and not hasattr(
-        model, "prepare_decoder_input_ids_from_labels"
-    ):
-        logger.warning(
-            "label_smoothing is enabled but the `prepare_decoder_input_ids_from_labels` method is not defined for"
-            f"`{model.__class__.__name__}`. This will lead to loss being calculated twice and will take up more memory"
-        )
 
     def preprocess_function(examples):
         inputs = [ex[source_lang] for ex in examples["translation"]]
@@ -164,30 +107,20 @@ def main():
         model_inputs = tokenizer(
             inputs,
             max_length=data_args.max_source_length,
-            padding=padding,
+            padding=False,
             truncation=True,
         )
 
         # Setup the tokenizer for targets
         with tokenizer.as_target_tokenizer():
             labels = tokenizer(
-                targets, max_length=max_target_length, padding=padding, truncation=True
+                targets, max_length=max_target_length, padding=False, truncation=True
             )
-
-        # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
-        # padding in the loss.
-        if padding == "max_length" and data_args.ignore_pad_token_for_loss:
-            labels["input_ids"] = [
-                [(l if l != tokenizer.pad_token_id else -100) for l in label]
-                for label in labels["input_ids"]
-            ]
 
         model_inputs["labels"] = labels["input_ids"]
         return model_inputs
 
     train_dataset = raw_datasets["train"]
-    if data_args.max_train_samples is not None:
-        train_dataset = train_dataset.select(range(data_args.max_train_samples))
     with training_args.main_process_first(desc="train dataset map pre-processing"):
         train_dataset = train_dataset.map(
             preprocess_function,
@@ -197,51 +130,12 @@ def main():
             desc="Running tokenizer on train dataset",
         )
 
-    # Data collator
-    label_pad_token_id = (
-        -100 if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id
+    data_collator = DataCollatorForSeq2Seq(
+        tokenizer,
+        model=model,
+        label_pad_token_id=tokenizer.pad_token_id,
+        pad_to_multiple_of=8 if training_args.fp16 else None,
     )
-    if data_args.pad_to_max_length:
-        data_collator = default_data_collator
-    else:
-        data_collator = DataCollatorForSeq2Seq(
-            tokenizer,
-            model=model,
-            label_pad_token_id=label_pad_token_id,
-            pad_to_multiple_of=8 if training_args.fp16 else None,
-        )
-
-    # Metric
-    metric = load_metric("sacrebleu")
-
-    def postprocess_text(preds, labels):
-        preds = [pred.strip() for pred in preds]
-        labels = [[label.strip()] for label in labels]
-
-        return preds, labels
-
-    def compute_metrics(eval_preds):
-        preds, labels = eval_preds
-        if isinstance(preds, tuple):
-            preds = preds[0]
-        decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
-        if data_args.ignore_pad_token_for_loss:
-            # Replace -100 in the labels as we can't decode them.
-            labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-
-        # Some simple post-processing
-        decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
-
-        result = metric.compute(predictions=decoded_preds, references=decoded_labels)
-        result = {"bleu": result["score"]}
-
-        prediction_lens = [
-            np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds
-        ]
-        result["gen_len"] = np.mean(prediction_lens)
-        result = {k: round(v, 4) for k, v in result.items()}
-        return result
 
     # Initialize our Trainer
     trainer = Seq2SeqTrainer(
@@ -251,46 +145,21 @@ def main():
         eval_dataset=None,
         tokenizer=tokenizer,
         data_collator=data_collator,
-        compute_metrics=compute_metrics
-        if training_args.predict_with_generate
-        else None,
     )
 
     # Do training
-    checkpoint = None
-    if training_args.resume_from_checkpoint is not None:
-        checkpoint = training_args.resume_from_checkpoint
-    elif last_checkpoint is not None:
-        checkpoint = last_checkpoint
+    checkpoint = last_checkpoint or None
     train_result = trainer.train(resume_from_checkpoint=checkpoint)
-    trainer.save_model()  # Saves the tokenizer too for easy upload
+    trainer.save_model()
 
+    # Output metrics
     metrics = train_result.metrics
-    max_train_samples = (
-        data_args.max_train_samples
-        if data_args.max_train_samples is not None
-        else len(train_dataset)
-    )
+    max_train_samples = len(train_dataset)
     metrics["train_samples"] = min(max_train_samples, len(train_dataset))
 
     trainer.log_metrics("train", metrics)
     trainer.save_metrics("train", metrics)
     trainer.save_state()
-
-    # Evaluation
-    results = {}
-
-    kwargs = {"finetuned_from": model_args.model_name_or_path, "tasks": "translation"}
-
-    languages = [
-        l for l in [data_args.source_lang, data_args.target_lang] if l is not None
-    ]
-    if len(languages) > 0:
-        kwargs["language"] = languages
-
-    trainer.create_model_card(**kwargs)
-
-    return results
 
 
 if __name__ == "__main__":
