b8f2faf3d7241fed58b4388d0ebef2c048792465
Gaurang Tandon <1gaurangtandon@gmail.com>
2021-12-11 13:32:48 +0530

    Got fetch-response working

--- webapp/app/app.py
+++ webapp/app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
+import json
 
 app = Flask(__name__)
 
@@ -10,16 +11,13 @@ def health():
 def translator():
     return render_template("index.html")
 
-@app.route("/translate", methods=["GET"])
-def translation_page():
-    return render_template("translate.html")
-
 @app.route("/translate", methods=["POST"])
 def get_translation():
-    sentence = request.form["sentence"]
+    data = json.loads(request.data.decode())
+    sentence = data["sentence"]
     # translate sentence
 
-    return redirect(url_for("translate.html"))
+    return sentence
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
