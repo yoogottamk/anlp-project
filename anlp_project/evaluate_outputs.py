23c320893c808aae4a23ae81d9c56e3fa08cfe4e
yoogottamk <yoogottamk@outlook.com>
2021-12-11 23:08:53 +0530

    add cli argument for easier runs

--- anlp_project/evaluate_outputs.py
+++ anlp_project/evaluate_outputs.py
@@ -1,8 +1,26 @@
+import sys
+
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from tqdm.auto import tqdm
 from sacrebleu.metrics import BLEU
 
 from anlp_project.datasets.europarl import EuroParlRaw
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} CHECKPOINT_PATH")
+    sys.exit(1)
+
+checkpoint_name = sys.argv[1]
+
+if "en-de" in checkpoint_name:
+    prompt = "translate from English to German: "
+    source_idx = 1
+elif "de-en" not in checkpoint_name:
+    prompt = "translate from German to English: "
+    source_idx = 0
+else:
+    print("Checkpoint name doesn't have en-de or de-en. Can't figure out direction")
+    sys.exit(1)
 
 # Initialize the tokenizer
 tokenizer = AutoTokenizer.from_pretrained("/scratch/en-de/")
@@ -20,10 +38,12 @@ refs = [[]]
 hyps = []
 for i in tqdm(range(N // batch_size)):
     batch = []
-    for x in range(i * batch_size, (i+1) * batch_size):
-        batch.append(f"translate English to German: {ds[x][1]}")
-        refs[0].append(ds[x][0])
-    tokenized_text = tokenizer(batch, padding="max_length", truncation=True, return_tensors="pt")
+    for x in range(i * batch_size, (i + 1) * batch_size):
+        batch.append(f"{prompt}: {ds[x][source_idx]}")
+        refs[0].append(ds[x][1 - source_idx])
+    tokenized_text = tokenizer(
+        batch, padding="max_length", truncation=True, return_tensors="pt"
+    )
 
     # Perform translation and decode the output
     translation = model.generate(**tokenized_text)
